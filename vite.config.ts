29e02a0a90cdbdb430f41240a66ec2db64e52d26
abilbaby <abilkbaby7@gmail.com>
2026-08-02 12:44:19 +0530

    final commit

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,6 @@ export default defineConfig(({ command }) => ({
       server: { entry: "server" },
     }),
     viteReact(),
-    ...(command === "build" ? [nitro({ preset: "cloudflare-module" })] : []),
+    ...(command === "build" ? [nitro({ preset: "cloudflare-pages" })] : []),
   ],
 }));
